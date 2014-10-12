31e625d1c105174a606e0001935bfaa96ed3211a
udondan <deemes79@gmail.com>
2014-10-12 11:57:22 +0200

    Added jquery as dependency

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
   summary: "Visual connectivity for webapps with jsPlump",
-  version: "1.6.4_1",
+  version: "1.6.4_3",
   git: "https://github.com/udondan/meteor-jsPlump.git",
   environments: 'client'
 });
 
 Package.on_use(function (api) {
   api.versionsFrom("METEOR@0.9.0");
+   api.use('jquery', 'client');
   api.add_files('vendor/jsPlump/dist/js/jquery.jsPlumb-1.6.4.js', 'client');
 });
