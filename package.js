7acc3238189dc73fff6a6d3b19ce37c8b03dff93
udondan <deemes79@gmail.com>
2014-10-12 11:32:10 +0200

    Upgraded to jsPlump 1.6.4

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
   summary: "Visual connectivity for webapps with jsPlump",
+  version: "1.6.4_1",
+  git: "https://github.com/udondan/meteor-jsPlump.git",
   environments: 'client'
 });
 
 Package.on_use(function (api) {
-  api.add_files('vendor/jsPlump/dist/js/jquery.jsPlumb-1.6.2.js', 'client');
+  api.versionsFrom("METEOR@0.9.0");
+  api.add_files('vendor/jsPlump/dist/js/jquery.jsPlumb-1.6.4.js', 'client');
 });
